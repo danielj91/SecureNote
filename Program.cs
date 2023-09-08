2468dc0f2a8e8445fefea3acd4ff77d80b2d1fc5
Daniel <dadanner@gmail.com>
2023-09-08 12:58:26 +0200

    Full cleanup

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 // Postgres setup
 builder.Services.AddDbContextFactory<SecureNoteContext>(options =>
